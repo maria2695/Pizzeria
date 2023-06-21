14893c24405fae2a6a291df61dc9bbe0ec56472c
Maria Lazareva <m.lazareva20040625@gmail.com>
2023-06-21 16:47:43 +0300

    debug(rename)

--- project/src/main/java/com/pizzeria/project/repository/ClientRepository.java
+++ project/src/main/java/com/pizzeria/project/repository/ClientRepository.java
@@ -1,10 +1,10 @@
 package com.pizzeria.project.repository;
 
-import ch.qos.logback.core.net.server.Client;
+import com.pizzeria.project.model.Clients;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Clients, Long> {
 
 }
--- project/src/main/java/com/pizzeria/project/repository/TransactionsRepository.java
+++ project/src/main/java/com/pizzeria/project/repository/TransactionsRepository.java
@@ -1,6 +1,5 @@
 package com.pizzeria.project.repository;
 
-import com.pizzeria.project.model.Drinks;
 import com.pizzeria.project.model.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
